d09e07f8d4b0162c31a61f4bf1840476586de31c
ENE MIHAELA <mihaela.een@gmail.com>
2023-10-20 18:56:54 +0300

    add, delete, update

--- src/main/java/ro/mycode/crudapp/CrudAppApplication.java
+++ src/main/java/ro/mycode/crudapp/CrudAppApplication.java
@@ -1,11 +1,18 @@
 package ro.mycode.crudapp;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import ro.mycode.crudapp.masina.models.Masina;
 import ro.mycode.crudapp.masina.repository.MasinaRepo;
+import ro.mycode.crudapp.masina.view.View;
+
+import java.beans.Transient;
+import java.util.Arrays;
+import java.util.Optional;
 
 @SpringBootApplication
 @Slf4j
@@ -16,18 +23,18 @@ public class CrudAppApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(MasinaRepo masinaRepo){
+	CommandLineRunner commandLineRunner(MasinaRepo masinaRepo , View view){
 		return  args->{
-
-
-			  masinaRepo.getAllMarci().stream().forEach(marca->{
-				  log.info(marca);
-			  });//din lista acceseaza eleemnt cu elemnt si executa codul
-
+	 view.play();;
+//			Optional<Masina> masina=masinaRepo.findMasinaByMarcaAndModel("Q",1999);
+//			if(masina.isPresent()){
+//				System.out.println(masina.get());
+//			}else{
+//
+//				System.out.println("Masina nu exista");
+//			}
 
 		};
-
-
 	}
 
 }
